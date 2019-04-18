b7ce04d1b997c73a3c9335370a599d5bf344c83c
Brandon Chapman <bchapman@e1z2r1p4.42.us.org>
2019-04-18 15:20:21 -0700

    current state

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bchapman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 14:06:56 by bchapman          #+#    #+#             */
-/*   Updated: 2019/04/14 13:45:51 by bchapman         ###   ########.fr       */
+/*   Updated: 2019/04/18 15:16:27 by bchapman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,34 @@
 #include "stdio.h"
 
 
-void	parse_flags(t_line *line, const char *p)
+void	parse_flags(t_line *line, const char **p)
+
 {
-	while (*p == '+' || *p == '-' || *p == '0' || *p == '#' || *p == ' ')
+	while (**p == '+' || **p == '-' || **p == '0' || **p == '#' || **p == ' ')
 	{
-		if (*p == '+')
+		if (**p == '+')
 		{
 			line->plus = 1;
-	//		line->length += 2;
 		}
-		else if (*p == '-')
+		else if (**p == '-')
 		{
 			line->minus = 1;
-	//		line->length += 2;
 		}
-		else if (*p == '0')
+		else if (**p == '0')
 		{
 			line->zero = 1;
 		}
-		else if (*p == '#')
+		else if (**p == '#')
 		{
 			line->pound = 1;
 		}
-		else if (*p == ' ')
+		else if (**p == ' ')
 		{
 			line->space = 1;
 		}
 		p++;
 	}
-	p++;
+//	p++;
 }
 
 void	parse_width(t_line *line, const char *p)
@@ -64,17 +63,17 @@ void	parse_width(t_line *line, const char *p)
 		line->width_digits = i;
 }
 
-void	parse_type(t_line *line, const char *p)
+void	parse_type(t_line *line, const char **p)
 {
 //	p++;
-	if (*p == 'd' || *p == 'i' || *p == 'o'
-			|| *p == 'u' || *p == 'x' || *p == 'X')
-		parse_dioux(line, p);
-	else if (*p == 'c' || *p == 's' || *p == 'p')
-		line->length += parse_csp(line->ap, p);
-	else if (*p == 'f')
+	if (**p == 'd' || **p == 'i' || **p == 'o'
+			|| **p == 'u' || **p == 'x' || **p == 'X')
+		parse_dioux(line, *p);
+	else if (**p == 'c' || **p == 's' || **p == 'p')
+		line->length += parse_csp(line->ap, *p);
+	else if (**p == 'f')
 		line->length += parse_float(line->ap);
-	else if (*p == '%')
+	else if (**p == '%')
 	{
 		ft_putchar('%');
 		line->length++;
@@ -97,28 +96,29 @@ void	init_line(t_line *line)
 int		ft_printf(const char *str, ...)
 {
 	int			i;
-	const char	*p;
 	t_line		*line;
 
 	line = (t_line*)ft_memalloc(sizeof(t_line));
 	init_line(line);
 	va_start(line->ap, str);
-	p = str;
-	while (*p != '\0')
+	while (*str != '\0')
 	{
-		if (*p != '%')
+		if (*str != '%')
 		{
-			write(1, p, 1);
+			write(1, str, 1);
 			line->length++;
-			p++;
+			str++;
 		}
 		else
 		{
-			p++;
-			parse_width(line, p);
-			parse_flags(line, p);
-			parse_type(line, p);
-			p++;
+			str++;
+
+			parse_flags(line, &str);
+//			parse_width(line, p);
+//			parse_precision(line, p);
+//			parse_length(line, p);
+			parse_type(line, &str);
+			str++;
 		}
 	}
 	i = line->length;
